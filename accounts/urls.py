9b23c6d98c41d8a26e7e9af3379d5da313ea366d
Tahdi Islam <tshdi60@gmail.com>
2024-05-30 23:35:42 +0600

    completed both practice day task

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('profile/', views.UserUpdateView.as_view(), name='profile'),
+    path('change-password/', views.change_pass, name='change_password')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,20 @@ from django.contrib.auth import login
 from django.views.generic import FormView, View
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
+from transactions.views import send_transaction_email
+# from django.core.mail import EmailMultiAlternatives
+# from django.template.loader import render_to_string
+
+# #  send email
+# def send_email(user, amount, subject, template):
+#     message = render_to_string(template, {'user': user})
+#     send_email = EmailMultiAlternatives(subject, '', to=[user.email])
+#     send_email.attach_alternative(message, 'text/html')
+#     send_email.send()
 
 # Create your views here.
 class UserRegistrationView(FormView):
@@ -46,3 +60,17 @@ class UserUpdateView(View):
             'title': 'Profile'
         })
         return context
+
+@login_required
+def change_pass(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Password change successfully')
+            update_session_auth_hash(request, form.user)
+            send_transaction_email(request.user, 0, 'Password Change Confirmation', 'accounts/password_change_email.html' )
+            return redirect('profile')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/change_password.html', {'form':form})
